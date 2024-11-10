297a1c0e6375bdc3dec31b4873fdc15edddc93b9
João Vitor <joaovitorfelixcontato@gmail.com>
2024-11-09 21:22:20 -0300

    feat: add initial setup routing

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func HandleHelloWorld(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
 
 func main() {
 	fmt.Println("Program starting...")
 
-	//Start the app here:
+	http.HandleFunc("/", HandleHelloWorld)
+
+	http.ListenAndServe(":8081", nil)
 }
